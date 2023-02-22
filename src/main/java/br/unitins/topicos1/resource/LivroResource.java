bba947ad6eb2c451e6f82f17872fca4ba1a1a8dc
Hítalo Aguiar <hitalo.aguiar3767@gmail.com>
2023-02-21 22:54:57 -0300

    Implementação do método PUT

--- src/main/java/br/unitins/topicos1/resource/LivroResource.java
+++ src/main/java/br/unitins/topicos1/resource/LivroResource.java
@@ -6,7 +6,9 @@ import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -30,5 +32,27 @@ public class LivroResource {
         livro.persist();
 
         return livro;
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Livro update (@PathParam ("id") Long id, Livro livro) {
+
+        Livro entity = Livro.findById(id);
+
+        entity.setTitulo(livro.getTitulo());
+
+        entity.setGenero(livro.getGenero());
+
+        entity.setIsbn(livro.getIsbn());
+
+        entity.setDataDeLancamento(livro.getDataDeLancamento());
+
+        entity.setNomeDoAutor(livro.getNomeDoAutor());
+
+        return entity;
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,11 @@
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-1');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-2');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-3');
+
+insert into Livro (id, titulo, genero, isbn, dataDeLancamento, nomeDoAutor)
+values (nextval('hibernate_sequence'), 'Vinte Mil Léguas Submarinas', 'ficção científica', '1233214566549',
+        '1869-09-22', 'Júlio Verne');
+
+insert into Livro (id, titulo, genero, isbn, dataDeLancamento, nomeDoAutor)
+values (nextval('hibernate_sequence'), 'Don Quixote', 'aventura', '0988907655672', '1603-10-07',
+        'Miguel de Cervantes');
